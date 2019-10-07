d67bce61b2d8cbdaa2579470e6452b057f64138c
Lag <lagaillardie.nicolas@live.fr>
2019-10-07 17:41:11 +0100

    Ownership

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,30 @@ fn main() {
     if_let();
 
     loop_test();
-}
+
+    ownership();
+
+    let s = String::from("hello");  // s comes into scope
+
+    takes_ownership(s);             // s's value moves into the function...
+    // ... and so is no longer valid here
+
+    let x = 5;                      // x comes into scope
+
+    makes_copy(x);                  // x would move into the function,
+    // but i32 is Copy, so it’s okay to still
+    // use x afterward
+
+    let _s1 = gives_ownership();         // gives_ownership moves its return
+    // value into s1
+
+    let s2 = String::from("hello");     // s2 comes into scope
+
+    let _s3 = takes_and_gives_back(s2);  // s2 is moved into
+    // takes_and_gives_back, which also
+    // moves its return value into s3
+} // Here, s3 goes out of scope and is dropped. s2 goes out of scope but was
+// moved, so nothing happens. s1 goes out of scope and is dropped.
 
 fn var(){
     // Trying variable mutable
@@ -41,6 +64,12 @@ fn var(){
     // spaces = spaces.len();
 
     println!("Number of spaces: {}", spaces);
+
+    let s1 = String::from("hello");
+
+    let (s2, len) = calculate_length(s1);
+
+    println!("The length of '{}' is {}.", s2, len);
 }
 
 fn data_types(){
@@ -207,4 +236,62 @@ fn loop_test(){
         println!("{}!", number);
     }
     println!("LIFTOFF!!!");
+}
+
+// Test ownership()
+fn ownership(){
+    let mut s = String::from("Hello");
+
+    s.push_str(" ,world"); // push_str() appends a literal to a String
+
+    println!("{}", s);
+
+    // Interactions
+    let x = 5;
+    let y = x; // y <- 5, copy the value
+
+    println!("x = {}, y = {}", x, y);
+
+    let s1 = String::from("Hello");
+    let _s2 = s1; // Points to the same memory location, doesn't create another String
+
+    // println!("{}, world", s1); // Doesn't work
+
+    let s3 = String::from("Hello");
+    let s4 = s3.clone(); // Clone the value
+
+    println!("s1 = {}, s2 = {}", s3, s4);
+}
+
+fn takes_ownership(some_string: String) { // some_string comes into scope
+    println!("{}", some_string);
+} // Here, some_string goes out of scope and `drop` is called. The backing
+// memory is freed.
+
+fn makes_copy(some_integer: i32) { // some_integer comes into scope
+    println!("{}", some_integer);
+} // Here, some_integer goes out of scope. Nothing special happens.
+
+fn gives_ownership() -> String {             // gives_ownership will move its
+    // return value into the function
+    // that calls it
+
+    let some_string = String::from("hello"); // some_string comes into scope
+
+    some_string                              // some_string is returned and
+    // moves out to the calling
+    // function
+}
+
+// takes_and_gives_back will take a String and return one
+fn takes_and_gives_back(a_string: String) -> String { // a_string comes into
+    // scope
+
+    a_string  // a_string is returned and moves out to the calling function
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len(); // len() returns the length of a String
+
+    (s, length)
 }
