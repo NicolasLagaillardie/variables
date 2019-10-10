86ff14d20aceb4034e0f58896ae737a2ee45bb85
Lag <lagaillardie.nicolas@live.fr>
2019-10-10 17:23:22 +0100

    Manage Errors

--- src/main.rs
+++ src/main.rs
@@ -110,6 +110,8 @@ fn main() {
     manage_hash_map();
 
     test_crash();
+
+    test_errors();
 } // Here, s3 goes out of scope and is dropped. s2 goes out of scope but was
 // moved, so nothing happens. s1 goes out of scope and is dropped.
 
@@ -894,4 +896,61 @@ fn test_crash(){
 
     // Will panic
     // v[99];
+}
+
+use std::fs::File;
+use std::io::ErrorKind;
+
+fn test_errors(){
+    let f = File::open("hello.txt");
+
+    // Doesn't work because u32 not in the Result
+    // let f: u32 = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => panic!("Problem opening the file: {:?}", other_error),
+        },
+    };
+
+    // Simplifed block from all above
+    let f = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+
+    // Shortcut for Panic on Error using unwrap
+    let f = File::open("hello.txt").unwrap();
+
+    // Shortcut for Panic on Error using expect
+    let f = File::open("hello.txt").expect("Failed to open hello.txt");
+}
+
+use std::io;
+use std::io::Read;
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
 }
