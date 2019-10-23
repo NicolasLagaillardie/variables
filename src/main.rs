28e3b69ba9d9e0f93c7e0e44d4ee84e8da7c897c
Lag <lagaillardie.nicolas@live.fr>
2019-10-23 15:03:32 +0100

    Tried MQTT
    Fail import craft cause missing cmake

--- src/main.rs
+++ src/main.rs
@@ -1192,6 +1192,10 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     }
 }
 
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
 fn test_lifetime(){
     let string1 = String::from("long string is long");
 
@@ -1200,4 +1204,49 @@ fn test_lifetime(){
         let result = longest(string1.as_str(), string2.as_str());
         println!("The longest string is {}", result);
     }
+
+    // &i32;        // a reference
+    // &'a i32;    // a reference with an explicit lifetime
+    // &'a mut i32;  // a mutable reference with an explicit lifetime
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.')
+    .next()
+    .expect("Could not find a '.'");
+    let i = ImportantExcerpt { part: first_sentence };
 }
+
+use std::process;
+
+// extern crate paho_mqtt as mqtt;
+//
+// fn test_paho(){
+//     // Create a client & define connect options
+//     let cli = mqtt::Client::new("tcp://farmer.cloudmqtt.com:14859").unwrap_or_else(|err| {
+//         println!("Error creating the client: {:?}", err);
+//         process::exit(1);
+//     });
+//
+//     let conn_opts = mqtt::ConnectOptionsBuilder::new()
+//     .keep_alive_interval(Duration::from_secs(20))
+//     .clean_session(true)
+//     .finalize();
+//
+//     // Connect and wait for it to complete or fail
+//     if let Err(e) = cli.connect(conn_opts).wait() {
+//         println!("Unable to connect:\n\t{:?}", e);
+//         process::exit(1);
+//     }
+//
+//     // Create a message and publish it
+//     let msg = mqtt::Message::new("test", "Hello world!");
+//     let tok = cli.publish(msg);
+//
+//     if let Err(e) = tok.wait() {
+//         println!("Error sending message: {:?}", e);
+//     }
+//
+//     // Disconnect from the broker
+//     let tok = cli.disconnect();
+//     tok.wait().unwrap();
+// }
